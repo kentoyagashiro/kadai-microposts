dc713e6808fe61cfb390a049a81458bfcdeb41d5
kento.yagashiro <winwincars@gmail.com>
2022-01-13 06:05:33 +0000

    dependent-destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,9 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   
   validates :content, presence: true, length: { maximum: 255 }
   
-   has_many :favorites
+   has_many :favorites, foreign_key: "micropost_id", dependent: :destroy
    has_many :favers,through: :favorites, source: :user
   
 end
